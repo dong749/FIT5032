4c68165f32deac6094a171cd9d0d2a2a47b8da46
unknown <renxudong749@gmail.com>
2024-08-02 14:55:53 +1000

    Week1

---
+++ src/components/MyName.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Welcome: {{ name }}
+    <br />
+    Xudong Ren's GitHub https://github.com/dong749/FIT5032.git
+  </div>
+</template>
+
+<script setup>
+const name = 'Xudong Ren'
+</script>
+
+<style scoped>
+div {
+  font-size: 20px;
+  color: #42b983;
+}
+
+a {
+  color: #007bff;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
